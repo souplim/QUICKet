97d1e7ec8f4d828c12a566b4ea94b558342114d5
eunja2 <ej0514@hanmail.net>
2023-03-17 14:44:04 +0900

    project template

---
+++ quicketproject/src/main/java/com/spring/QuicketprojectApplication.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicketprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuicketprojectApplication.class, args);
+	}
+
+}
