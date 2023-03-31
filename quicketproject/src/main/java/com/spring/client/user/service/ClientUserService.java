7ccb4c6d6b0b89ac5de86013e600484210f9ec3c
kimnjbono <summerpuppy94@gmail.com>
2023-03-31 10:34:52 +0900

    LoginUser 복사한 것

---
+++ quicketproject/src/main/java/com/spring/client/user/service/ClientUserService.java
@@ -0,0 +1,18 @@
+package com.spring.client.user.service;
+
+//import java.util.List;
+
+import com.spring.client.user.vo.UserVO;
+
+public interface ClientUserService {
+	public UserVO userLoginProcess(UserVO login);
+	public UserVO userInfo(UserVO uvo);
+	/*public int userJoin(UserVO user) throws Exception;
+	public int setNewPwd(UserVO user) throws Exception;
+	public int userUpdate(UserVO user) throws Exception;
+	public int userDelete(UserVO user) throws Exception;
+	public List<UserVO> searchIdPhone(String u_name, String u_phone);
+	public List<UserVO> searchIdEmail(String u_name, String u_email);
+	public int userConfirm(String u_name, String u_id, String u_email);
+	*/
+}
