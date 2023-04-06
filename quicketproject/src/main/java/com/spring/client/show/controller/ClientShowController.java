8eab5562d6251e940d350e6d6890b65d7d9531f2
eunja2 <ej0514@hanmail.net>
2023-04-06 18:58:06 +0900

    로그인 인터셉터 수정

--- quicketproject/src/main/java/com/spring/client/show/controller/ClientShowController.java
+++ quicketproject/src/main/java/com/spring/client/show/controller/ClientShowController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.spring.admin.ticket.vo.HallVO;
